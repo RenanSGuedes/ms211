c13dcc551d516041159e162930882eb879cd5181
RenanSGuedes <renanguedes.br@gmail.com>
2020-12-16 01:25:20 -0300

    :horse: With debug lines

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from sympy.abc import x, y, _clash1
 from sympy import sympify, expand, simplify
 
-coordinates = [(1, 0), (2, 1), (3, 4)] 
-index = [0, 1, 2] # 0, 1, 2
+coordinates = [(1, 0), (2, 1), (3, 4), (5,6), (7,8)] 
+index = [0, 1, 2, 3, 4] # 0, 1, 2
 coordinatesRef = index.copy()
 l = []
 g = []
@@ -33,11 +33,11 @@ for j in range(len(index)):
   coordinatesRef = index.copy()
 
 
-  for i in range(lagrangeList):
-    pList.append(lagrangeList[i] * coordinates[i][1])
-   
-  
-  print(pList)
+for i in range(len(lagrangeList)):
+  pList.append(lagrangeList[i] * coordinates[i][1])
+  result = expand("+".join(str(x) for x in pList))
 
+print("+".join(str(x) for x in pList))
+print(simplify(result))
 
 
